ae2f941a950cdbcb24044eb15b220d407e0cddcd
Fall Fatou <fallfatou331@gmail.com>
2023-04-03 15:23:02 +0100

    update

--- portfolio/js/weather/app.js
+++ portfolio/js/weather/app.js
@@ -1,14 +1,16 @@
+document.addEventListener("DOMContentLoaded", populatetableRows);        
+
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1)
-function populatetableRows() {
-    async function populatetableRows() {
-        await fetch('https://api.openweathermap.org/data/2.5/weather?q=Dublin&units=metric&appid=9b3b5c5b5c5b5c5b5c5b5c5b5c5b5c5b')
-     
-           .then(response => {
-              if (response.status !== 200) {
-                 console.log('Error Status Code: ' + response.status);
-                 return;
-              }
-              response.json().then((data) => {
+
+async function populatetableRows() { 
+    await fetch('https://api.openweathermap.org/data/2.5/weather?q=dublin,ie&units=metric&APPID=bc425ac2188d406c884f4fdd88b339f0')
+
+    .then(response => {
+        if (response.status !== 200) {
+            console.log('Error Status Code: ' + response.status);
+            return;
+        }
+        response.json().then((data) => {
                 let strTableRows = `<tr>
                     <td><span>Summary</span></td>
                     <td>${capitalize(data["weather"][0]["description"])}</td>
@@ -24,23 +26,26 @@ function populatetableRows() {
                 <tr>
                     <td><span>Pressure</span></td>
                     <td>${data["main"]["pressure"] + " Pa"}</td>
-                </tr>`;	
-                document.addEventListener("DOMContentLoaded", populatetableRows);
-});
-})
-           .catch(error => {
-            // handle any error
-        });
-    }   
-} 
+                </tr>`;
+                document.querySelector("#table-weather-dublin tbody").innerHTML = strTableRows;
+            });
+           
+        })
 
-function change_background(){
+        .catch(error => {
+            // handle any error
+       });
+
+}
+
+function change_background() {
     let d = new Date();
     let n = d.getHours();
-    if (n > 23 || n <= 6){
-        document.querySelector(".theme.js").style.backgroundImage = "url('assets/images/dublin-night.jpg')";
-    }else{
-        document.querySelector(".theme.js").style.backgroundImage = "url('assets/images/dublin-day.jpg')";
+    if (n > 23 || n <= 6) {
+        document.querySelector(".theme-js").style.backgroundImage  ="url('assets/img/dublin-night.jpg')";
+    } else {
+        document.querySelector(".theme-js").style.backgroundImage  ="url('assets/img/dublin-day.jpg')";
     }
 }
+
 change_background();
